7f286f0df1ba6a0f3eff75d51327ba262efc75db
Jromanosorio <jandres.roman.08@gmail.com>
2025-11-20 17:49:28 -0500

    api call

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,84 @@
-import Image from "next/image";
+"use client"
+
+import { useEffect, useState } from "react";
+import { FaRegClipboard, FaScissors } from "react-icons/fa6";
+import { shortURL } from "./services/api.service";
+import { useLocalStorage } from "./hooks/useLocalStorage";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [link, setLink] = useState("")
+  const [list, setList] = useState<any[]>([])
+  const { storedValue, setValue, removeValue } = useLocalStorage<any[]>("shortenedLinks", [])
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  const getShortUrl = async (e: any) => {
+    e.preventDefault()
+
+    const data = await shortURL(link)
+
+    if (!list.some((item: any) => item.shortedLink === data.shortedLink)) {
+      setList([...list, data])
+      setValue([...list, data])
+    }
+
+    setLink("")
+  }
+
+  useEffect(() => {
+    setList(storedValue)
+  }, [storedValue])
+
+  return (
+    <div className="flex items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-dark">
+      <main className="main mx-auto">
+        <div>
+          <h1 className="text-green text-6xl! font-orbitron! font-semibold">URL Shortener</h1>
+          <p className="text-white font-medium text-2xl">Acorta tus <span className="text-green">enlaces</span> al instante</p>
+          <form className="mt-8 flex gap-5">
+            <input
+              type="text"
+              name="link"
+              autoComplete="off"
+              className="min-w-md text-white border-2 outline-none py-3 px-3 border-gray-600 rounded-md focus:border-green transition easy duration-200"
+              placeholder="Pega aquí tu url..."
+              onChange={(e) => setLink(e.target.value)}
+              value={link}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <button type="submit" onClick={getShortUrl} className="bg-green py-3 px-4 font-bold flex items-center gap-3 rounded-md cursor-pointer hover:bg-green-600 transition easy duration-200">
+              <FaScissors /> Acortar URL
+            </button>
+          </form>
+        </div>
+        <div className="mt-5">
+          {
+            list.length > 0 ? (
+              <table className="w-full table-auto border-2 border-gray-600 text-white">
+                <thead>
+                  <tr className="border-b-2 border-gray-600">
+                    <th className="mx-auto py-1 px-3">Order</th>
+                    <th className="mx-auto py-1 px-3">URL Original</th>
+                    <th className="mx-auto py-1 px-3">URL Acortada</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                    list.map((item: any, idx: number) => {
+                      return (
+                        <tr key={idx}>
+                          <td className="p-2 text-center">{idx + 1}</td>
+                          <td className="p-2 max-w-md truncate text-start">{item.data.link}</td>
+                          <td className="py-2 px-5 max-w-md text-green text-start flex items-center justify-between gap-2">{item.shortedLink} <FaRegClipboard onClick={() => navigator.clipboard.writeText(item.shortedLink)} className="cursor-pointer text-gray-400 hover:text-green transition easy duration-200" /> </td>
+                        </tr>
+                      )
+                    })
+                  }
+                </tbody>
+              </table>
+            ) : (
+              <p className="text-white font-medium text-xl">No se han acortado enlaces</p>
+            )
+          }
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
---
+++ app/services/api.service.ts
@@ -0,0 +1,16 @@
+const API_URL = process.env.API_URL || "http://localhost:3002/api/shorten"
+
+export const shortURL = async (url: string) => {
+
+    const response = await fetch(API_URL, {
+        method: 'POST',
+        body: JSON.stringify({ link: url }),
+        headers: {
+            "Content-Type": "application/json",
+        }
+    })
+
+    if (!response.ok) throw new Error("Error acortando el url");
+
+    return response.json()
+}
